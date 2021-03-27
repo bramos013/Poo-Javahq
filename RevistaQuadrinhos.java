0077d8ea7808053242d592a59ab01e895168e4fd
bramos013 <brunoramos013@gmail.com>
2021-03-27 20:29:48 -0300

    Removido atributo autor RevistaQuadrinho

--- RevistaQuadrinhos.java
+++ RevistaQuadrinhos.java
@@ -1,34 +1,30 @@
 public class RevistaQuadrinhos {
 
 	private String nome;	
-	private String autor;
 	private String marca;
 	private String colecao;
 	private int ano;
 	private Dono[] dono;
 
 	//Construtor de HQ padrão
-	public RevistaQuadrinhos(String nome, String autor, String marca, String colecao, int ano, Dono[] dono) {
+	public RevistaQuadrinhos(String nome, String marca, String colecao, int ano, Dono[] dono) {
 		this.nome = nome;
-		this.autor = autor;
 		this.marca = marca;
 		this.colecao = colecao;
 		this.ano = ano;
 		this.dono = dono;
 	}
 	//Construtor de HQ dos Marvel
-	public RevistaQuadrinhos(String nome, String autor, String colecao, int ano, Dono[] dono) {
+	public RevistaQuadrinhos(String nome, String colecao, int ano, Dono[] dono) {
 		this.nome = nome;
-		this.autor = autor;
 		this.marca = "Marvel";
 		this.colecao = colecao;
 		this.ano = ano;
 		this.dono = dono;
 	}
 	//Construtor de HQ dos Vingadores
-	public RevistaQuadrinhos(String nome, String autor, int ano, Dono[] dono) {
+	public RevistaQuadrinhos(String nome, int ano, Dono[] dono) {
 		this.nome = nome;
-		this.autor = autor;
 		this.marca = "Marvel";
 		this.colecao = "Avengers";
 		this.ano = ano;
